a76f5cb0756c914f14c2c830d250d8a1d407fc1c
mahesh3567 <128398184+mahesh3567@users.noreply.github.com>
2024-01-10 08:51:40 +0530

    Add files via upload

---
+++ easy/vlaidusername.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class vlaidusername{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a username: ");
+        String username = scanner.nextLine();
+
+        scanner.close();
+
+        if (isValidUsername(username)) {
+            System.out.println("Valid username!");
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+    private static boolean isValidUsername(String username) {
+        if (username.length() >= 4 && username.length() <= 20) {
+            if (username.matches("[a-zA-Z0-9]+")) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
