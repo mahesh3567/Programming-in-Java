fce678eda7736c7c8b82bb4fd6682fa15d4c8cf4
mahesh3567 <128398184+mahesh3567@users.noreply.github.com>
2024-01-11 14:03:34 +0530

    Update and rename 03.vlaidusername.java to 03.validusername.java

--- easy/03.validusername.java
+++ easy/03.validusername.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class vlaidusername{
+public class validusername{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
