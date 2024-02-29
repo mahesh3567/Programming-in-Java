e505aa2e989c7bac76088f4859bc7d8fd901c54e
mahesh3567 <128398184+mahesh3567@users.noreply.github.com>
2024-02-29 09:57:56 +0530

    Add files via upload

---
+++ 28-02-2024/19.UUID.java
@@ -0,0 +1,12 @@
+import java.util.UUID;
+
+public class UUIDGenerator {
+
+    public static void main(String[] args) {
+        // Generating a random UUID
+        UUID uuid = UUID.randomUUID();
+
+        // Displaying the generated UUID
+        System.out.println("Random UUID: " + uuid);
+    }
+}
